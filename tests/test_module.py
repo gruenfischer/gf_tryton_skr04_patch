96615f78fa617d9189cce91e5b62f2d253103c0f
Jakob Fischer <jakob.fischer@gruenfischer.de>
2025-01-22 13:47:56 +0100

    initial commit

---
+++ tests/test_module.py
@@ -0,0 +1,10 @@
+
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class TrytonSkr04PatchTestCase(ModuleTestCase):
+    "Test Tryton Skr04 Patch module"
+    module = 'tryton_skr04_patch'
+
+
+del ModuleTestCase
